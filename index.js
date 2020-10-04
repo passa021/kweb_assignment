5c60314ccfd2cc89a8560636a8e55fbafde6e73d
mimThekim <passa021@korea.ac.kr>
2020-10-04 14:57:53 +0900

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http');
-const hostname = '127.0.0.1'; const port = 3000;
-const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end('Hello World\n');
+const hostname = '127.0.0.1'; const port = 4000;
+
+function clock(){
+    var d = new Date();
+    var hour = d.getHours().toString();
+    var minute = d.getMinutes().toString();
+    var second = d.getSeconds().toString();
+
+    NOW += hour + ':' + minute + ':' + second ;
+    
+    return NOW;
+}
+
+const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end(clock());
 });
+
+
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
 });
